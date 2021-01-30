3c7ecbc012542827acdaedf6e6066df2d88b2485
Akira Hasegawa <akira.hasegawa@jaist.ac.jp>
2021-01-31 01:59:27 +0900

    Improve readability

--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,42 @@ struct Doc {
     memo: String,
 }
 
-fn read_json<P: AsRef<Path>>(path: P) -> Result<Vec<Doc>, Box<dyn Error>> {
-    // reference: https://docs.serde.rs/serde_json/de/fn.from_reader.html (2021-01-30)
-    let file = std::fs::File::open(path)?;
-    let reader = std::io::BufReader::new(file);
-    let doc = serde_json::from_reader(reader)?;
-    Ok(doc)
+trait Json: Sized {
+    fn read_json<P: AsRef<Path>>(path: P) -> Result<Self, Box<dyn Error>>;
+    fn write_json<P: AsRef<Path>>(&self, path: P) -> Result<(), Box<dyn Error>>;
+    fn add_memo(&mut self, a_name: String, a_memo: String);
+    fn find_doc(&self, a_name: String) -> Option<&Doc>;
 }
-
-fn write_json<P: AsRef<Path>>(path: P, json: Vec<Doc>) -> Result<(), Box<dyn Error>> {
-    use std::io::Write;
-    let mut file = std::fs::File::create(path)?;
-    file.write_all(serde_json::to_string_pretty(&json)?.as_bytes())?;
-    Ok(())
-}
-
-fn add_memo(json: &mut Vec<Doc>, a_name: String, a_memo: String) {
-    if json.iter().all(|x| !x.name.eq(&a_name)) {
-        json.push(Doc {
-            name: a_name,
-            memo: a_memo,
-        });
-    } else {
-        eprintln!("Duplicated name is found: {}", a_name);
+impl Json for Vec<Doc> {
+    fn read_json<P: AsRef<Path>>(path: P) -> Result<Self, Box<dyn Error>> {
+        // reference: https://docs.serde.rs/serde_json/de/fn.from_reader.html (2021-01-30)
+        let file = std::fs::File::open(path)?;
+        let reader = std::io::BufReader::new(file);
+        let doc = serde_json::from_reader(reader)?;
+        Ok(doc)
     }
-}
 
-fn find_doc(json: &Vec<Doc>, a_name: String) -> Option<&Doc> {
-    json.iter().find(|&x| x.name.eq(&a_name))
+    fn write_json<P: AsRef<Path>>(&self, path: P) -> Result<(), Box<dyn Error>> {
+        use std::io::Write;
+        let mut file = std::fs::File::create(path)?;
+        file.write_all(serde_json::to_string_pretty(&self)?.as_bytes())?;
+        Ok(())
+    }
+
+    fn add_memo(&mut self, a_name: String, a_memo: String) {
+        if self.iter().all(|x| !x.name.eq(&a_name)) {
+            self.push(Doc {
+                name: a_name,
+                memo: a_memo,
+            });
+        } else {
+            eprintln!("Duplicated name is found: {}", a_name);
+        }
+    }
+
+    fn find_doc(&self, a_name: String) -> Option<&Doc> {
+        self.iter().find(|&x| x.name.eq(&a_name))
+    }
 }
 
 fn main() {
@@ -57,15 +65,15 @@ fn main() {
         .expect("invalid file name")
         .to_string();
 
-    let mut json = match read_json(&json_path) {
+    let mut json = match Json::read_json(&json_path) {
         Ok(json) => json,
         Err(_) => Vec::new(),
     };
 
     if let Some(new_memo) = env::args().nth(3) {
-        add_memo(&mut json, pdf_file_name, new_memo);
-        write_json(&json_path, json).expect("failed to write json");
-    } else if let Some(doc) = find_doc(&json, pdf_file_name) {
+        json.add_memo(pdf_file_name, new_memo);
+        json.write_json(&json_path).expect("failed to write json");
+    } else if let Some(doc) = json.find_doc(pdf_file_name) {
         println!("{}", doc.memo)
     } else {
         show_usage();
